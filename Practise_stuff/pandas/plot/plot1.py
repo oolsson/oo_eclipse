f5713b96140dbe715ef6c8f5aa7fb8d18772dbd8
oolsson <o.h.olsson@gmail.com>
2014-08-31 13:59:13 +1000

    added simple pandas plot

--- Practise_stuff/pandas/plot/plot1.py
+++ Practise_stuff/pandas/plot/plot1.py
@@ -5,15 +5,8 @@ import matplotlib.pyplot as plt
 # pd.options.display.mpl_style = 'default'
 
 
-# xl = pd.ExcelFile("C:\Users\oskar\Documents\GitHub\oo_eclipse\my_stuff\Data\Retail_turnover.xls")
-# x = xl.parse("Sheet1")
-# print x.head(10)
-# fig=x.plot()
-
 ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-
 ts = ts.cumsum()
 
 ts.plot()
 plt.show()
-
